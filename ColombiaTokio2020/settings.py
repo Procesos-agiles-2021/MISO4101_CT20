58c9652b4fcf3307defe752b344b7efaddb97e34
tdiaz1 <tj.diaz@uniandes.edu.co>
2021-02-07 22:16:52 -0500

    dependencies added

--- ColombiaTokio2020/settings.py
+++ ColombiaTokio2020/settings.py
@@ -124,6 +124,5 @@ STATIC_URL = '/static/'
 #location where django collect all static files
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
+# location where you will store your static files
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'ColombiaTokio2020/static')]
