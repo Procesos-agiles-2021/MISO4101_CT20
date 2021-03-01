d0c6936361bb35fcfa7193954c3ba38029fd0386
tdiaz1 <tj.diaz@uniandes.edu.co>
2021-02-28 19:05:02 -0500

    refactor structure for heroku deploy

--- portal/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path, include
-from . import views
-from django.conf.urls import url
-
-urlpatterns = [
-    path('', views.redirect_to_auth, name="Home"),
-    path('list/', views.list_object.as_view(), name="List_all"),
-    #url(r'adduser/$', views.add_user, name='addUser'),
-]
